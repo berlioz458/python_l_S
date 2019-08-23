b88ec7b74a4aa58d5dbb231ed178ec286c226dc9
shulinina.e <test-2@prodv.net>
2019-08-23 20:14:56 +0700

    fix sample 2

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 a = int(input())
 b = int(input())
 c = int(input())
@@ -7,17 +9,18 @@ print('', end='\t')
 
 for i in range(c, d):
     print(i, end="\t")
+
 print(d, end="\n")
 
 for i in range(a, b):
     for j in range(c, d):
-        print(i, end="\t")
-        print(i * j, end='\t')
+        print(i, end="\t"),
+        print(i * j, end='\t'),
         print(i * d, end='\t')
-
     print('', end='\n')
 
 print(b, end="\t")
 for i in range(c, d):
-    print(b * i, end='\t')
-    print(b * d, end='\t')
+    print(b * i, end='\t'),
+
+print(b * d, end='\t')
